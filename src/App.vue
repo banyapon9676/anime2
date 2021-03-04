6ae9109a0ec7d1c99c487775eca88694415221f3
banyapon9676 <banyapon9676@gmail.com>
2021-03-04 17:19:01 +0700

    master

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,60 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div
+    class="bg-dark"
+    bg-danger
+    style="
+      background-image: url('https://i.pinimg.com/originals/b5/be/43/b5be432ff77705296a32e7c3276c4919.gif');
+    "
+  >
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <img
+        overlay
+        src="https://f.ptcdn.info/409/013/000/1387207932-PedoZero-o.jpg"
+        alt="พี่หมีใส่สูท"
+        width="60px"
+        height="50px"
+      />
+      <h1 style="color: white">AnimeLo</h1>
+    </b-navbar>
+    <b-container fluid>
+      <div class="mt-5"></div>
+      <b-row>
+        <b-col  cols="3" v-for="anime in animelist.top" :key="anime">
+          <Anime  :title="anime.title" :image="anime.image_url" />
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Anime from "@/components/Anime.vue";
+import axios from "axios";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Anime,
+  },
+  data() {
+    return {
+      animelist: {},
+    };
+  },
+  mounted() {
+    axios({
+      method: "GET",
+      url: "https://jikan1.p.rapidapi.com/top/anime/1/upcoming",
+      headers: {
+        "x-rapidapi-key": "d070d378bbmshc8f19ecd0f7796dp11cf3djsn60b74ee1cda7",
+        "x-rapidapi-host": "jikan1.p.rapidapi.com",
+      },
+    }).then((result) => {
+      this.animelist = result.data;
+    });
+  },
+};
 </script>
 
-<style>
+<style >
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +62,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  background-color: rgb(255, 0, 0);
 }
 </style>
---
+++ src/components/Anime.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="card">
+    <b-card
+
+      :img-src="image"
+      img-alt="Card image"
+      img-top
+      tag="article"
+      class="overflow-hidden"
+      align="center"
+      bg-variant="dark"
+      style="height: 38rem"
+    >
+   
+      <b-card-text style="color: white">
+        <h4>{{ title }}</h4>
+      </b-card-text>
+    </b-card>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    title: String,
+    image: String,
+  },
+};
+</script>
